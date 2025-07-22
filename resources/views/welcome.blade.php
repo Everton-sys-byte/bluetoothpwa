5eb5248ff747c4b35452fec4c00cce1db4b0f65c
Everton Soares <everton_dev@hotmail.com>
2025-07-22 17:39:38 -0300

    download apk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
     <title>Bluetooth</title>
 
@@ -16,7 +18,9 @@
 </head>
 
 <body>
-    <p>oi</p>
+    <div class="container-full d-flex flex-column align-items-center justify-content-center" style="height: 100vh">
+        <a class="btn btn-primary" href="{{ asset('downloads/Bluetooth.apk') }}">Download .APK</a>
+    </div>
 </body>
 
 </html>
